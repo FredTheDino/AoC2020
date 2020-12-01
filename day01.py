467511296eacc9f279b989963a9502e63010e6bc
Edvard Thörnros <edvard.thornros@gmail.com>
2020-12-01 23:09:55 +0100

    Remove scribbles

--- day01.py
+++ day01.py
@@ -8,12 +8,6 @@ while True:
         break
     numbers.append(n)
 
-def comb(xs, n):
-    for i, x in enumerate(xs):
-        for res in comb(xs[i:], n - 1):
-            ...
-
-
 for a, b in combinations(numbers, 2):
     if a + b == 2020:
         print(a * b)
