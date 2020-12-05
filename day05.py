0eac472f12c59621c882df765e0abf1234d27936
Edvard Thörnros <edvard.thornros@gmail.com>
2020-12-05 11:22:58 +0100

    CRUSH day 5

---
+++ day05.py
@@ -0,0 +1,15 @@
+import sys
+
+seats = set()
+for line in sys.stdin.readlines():
+    seatid = line.replace("L", "0") \
+                 .replace("R", "1") \
+                 .replace("F", "0") \
+                 .replace("B", "1")
+    seats.add(int(seatid, 2))
+
+print(max(seats))
+
+for seat in seats:
+    if seat + 1 not in seats and seat + 2 in seats:
+        print(seat + 1)
